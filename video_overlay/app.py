6c01dcc8334db42de4a7e449f80b14e461cf25b6
Dipam Vasani <dipam44@gmail.com>
2021-05-19 12:28:19 -0400

    make changes for streamlit sharing
    add requirements to the root folder
    
    try debugging on share
    
    debugging #2
    
    debugging #3
    
    debugging #4
    
    write videos before overlaying
    
    pause before inference
    
    correct filenames
    
    check folder contents
    
    apply changes from discussion forum
    
    Initial commit for pdf editor
    
    final touches

--- video_overlay/app.py
+++ video_overlay/app.py
@@ -1,15 +1,24 @@
-import streamlit as st
+import base64
+import codecs
 import subprocess
 
-import os
-import base64
+import streamlit as st
+import streamlit.components.v1 as components
 
+
+# deprecated, not usable for larger files
 def get_binary_file_downloader_html(bin_file, file_label='File'):
     with open(bin_file, 'rb') as f:
         data = f.read()
     bin_str = base64.b64encode(data).decode()
-    href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{os.path.basename(bin_file)}">Download {file_label}</a>'
+    href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{bin_file}">Download {file_label}</a>'
     return href
+
+
+def download_button(html_file):
+    calc_file = codecs.open(html_file, 'r')
+    page = calc_file.read()
+    components.html(html=page, height=50)
 
 
 def get_mapping_dict(od_x: int, od_y: int):
@@ -21,10 +30,14 @@ def get_mapping_dict(od_x: int, od_y: int):
     }
     return overlay_mapping
 
-st.markdown("# Overlay one video over another")
-st.markdown('A utility tool to create lectures using screen recording and a video of a person talking.')
-st.markdown('Just choose a background and an overlay video and adjust parameters to place it where you like.')
-st.markdown('Once the output is generated a download option will appear at the bottom.')
+
+st.markdown("""
+    # Overlay one video over another
+    A utility tool to create lectures using screen recording and a video of a person talking.<br>
+    Just choose a background and an overlay video and adjust parameters to place it where you like.<br>
+    Once the output is generated right click to Download the output.
+    """,
+    unsafe_allow_html=True)
 
 positions = ['top-right', 'top-left', 'bottom-right', 'bottom-left']
 
@@ -34,11 +47,11 @@ pos = st.sidebar.selectbox("Select position: ", positions)
 h_label = "Horizontal distance of the overlayed video from left or right depending on placement"
 v_label = "Vertical distance of the overlayed video from top or bottom depending on placement"
 
-odist_x = st.sidebar.number_input(h_label, value = 50)
-odist_y = st.sidebar.number_input(v_label, value = 50)
+odist_x = st.sidebar.number_input(h_label, value=50)
+odist_y = st.sidebar.number_input(v_label, value=50)
 
-scale_x = st.sidebar.number_input("Width of overlayed video", value = 400)
-scale_y = st.sidebar.number_input("height of overlayed video", value = -1)
+scale_x = st.sidebar.number_input("Width of overlayed video", value=400)
+scale_y = st.sidebar.number_input("height of overlayed video", value=-1)
 
 back = st.file_uploader("Choose background video", type=["mp4", "mov"])
 over = st.file_uploader("Choose overlay video", type=["mp4", "mov"])
@@ -46,14 +59,27 @@ over = st.file_uploader("Choose overlay video", type=["mp4", "mov"])
 if back is not None and over is not None:
     bk = back.name
     ov = over.name
-    op_vid = f"overlay_op.mp4"
-    
+    with open(bk, mode='wb') as f:
+        f.write(back.read())
+    with open(ov, mode='wb') as f:
+        f.write(over.read())
+
+    # st.markdown(f"""
+    # ### Files
+    # - {bk}
+    # - {ov}
+    # """,
+    # unsafe_allow_html=True)
+
+    op_vid = "overlay_op.mp4"
     overlay_dict = get_mapping_dict(odist_x, odist_y)
-    cmd = f'ffmpeg -i {bk} -i {ov} -map 0:0 -map 1:1 -vf "movie={ov}, scale={scale_x}:{scale_y} [inner]; [in][inner] overlay={overlay_dict[pos]} [out]" {op_vid} -y'
-    op = subprocess.check_output(cmd, shell=True)
-    video_file = open(op_vid, 'rb')
-    video_bytes = video_file.read()
 
-    st.video(video_bytes)
+    if st.button('Overlay videos'):
+        with st.spinner('Processing videos...'):
+            cmd = f'ffmpeg -i {bk} -i {ov} -map 0:0 -map 1:1 -vf "movie={ov}, scale={scale_x}:{scale_y} [inner]; [in][inner] overlay={overlay_dict[pos]} [out]" {op_vid} -y'
+            op = subprocess.check_output(cmd, shell=True)
 
-    st.markdown(get_binary_file_downloader_html(f'{op_vid}', 'Video'), unsafe_allow_html=True)
+            st.video(op_vid)
+            st.balloons()
+            # download_button('videodownload.html')
+            # st.markdown(get_binary_file_downloader_html(op_vid, 'Video'), unsafe_allow_html=True)
