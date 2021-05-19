bdedb6d9abae77a1019aa7ca7a3444c246882683
Dipam Vasani <dipam44@gmail.com>
2021-05-19 14:21:33 -0400

    debugging #3

--- video_overlay/app.py
+++ video_overlay/app.py
@@ -47,8 +47,9 @@ if back is not None and over is not None:
     bk = back.name
     ov = over.name
     op_vid = f"overlay_op.mp4"
-    st.markdown(bk, ov)
+    st.markdown(f'{bk} {ov}')
     os.system('ls video_overlay')
+    os.system('cat /home/appuser/venv/lib/python3.7/site-packages/streamlit/script_runner.py')
     # overlay_dict = get_mapping_dict(odist_x, odist_y)
     # cmd = f'ffmpeg -i {bk} -i {ov} -map 0:0 -map 1:1 -vf "movie={ov}, scale={scale_x}:{scale_y} [inner]; [in][inner] overlay={overlay_dict[pos]} [out]" {op_vid} -y'
     # op = subprocess.check_output(cmd, shell=True)
