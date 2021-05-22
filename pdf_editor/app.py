0d8f35cfe4488240d41ab97b878f8e8240b7e4e2
Dipam Vasani <dipam44@gmail.com>
2021-05-21 22:02:16 -0400

    Initial commit for pdf editor

---
+++ pdf_editor/app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import base64
+
+def show_pdf(file_path):
+    with open(file_path,"rb") as f:
+          base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+    pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
+    st.markdown(pdf_display, unsafe_allow_html=True)
+
+show_pdf("sample_pdf.pdf")
